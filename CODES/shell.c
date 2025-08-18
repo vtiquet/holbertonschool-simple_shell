b0408d8fe8c32ae0d40c5be56c75b5e1ad143fc0
Schpser <schps.games@gmail.com>
2025-08-18 14:44:10 +0200

    Correcting PATH

--- CODES/shell.c
+++ CODES/shell.c
@@ -1,48 +1,6 @@
 #include "shell.h"
 
 /**
- * find_in_path - Finds command in PATH
- * @cmd: Command to find
- * Return: Full path or NULL
- */
-char *find_in_path(char *cmd)
-{
-	char *path, *path_copy, *dir, *full_path;
-	struct stat st;
-
-	if (strchr(cmd, '/'))
-	{
-		if (stat(cmd, &st) == 0)
-			return (strdup(cmd));
-		return (NULL);
-	}
-
-	path = getenv("PATH");
-	if (!path)
-		return (NULL);
-
-	path_copy = strdup(path);
-	dir = strtok(path_copy, ":");
-	while (dir)
-	{
-		full_path = malloc(strlen(dir) + strlen(cmd) + 2);
-		if (full_path)
-		{
-			printf(full_path, "%s/%s", dir, cmd);
-			if (stat(full_path, &st) == 0)
-			{
-				free(path_copy);
-				return (full_path);
-			}
-			free(full_path);
-		}
-		dir = strtok(NULL, ":");
-	}
-	free(path_copy);
-	return (NULL);
-}
-
-/**
  * cleanup - Free resources and reset pointers
  * @line: Pointer to input line
  * @args: Pointer to arguments array
