4b799071ba5b4aab4decdd6554b343e119e1f2ac
Schpser <schps.games@gmail.com>
2025-08-20 15:11:15 +0200

    PATH in helper.c

--- shell.h
+++ shell.h
@@ -31,10 +31,10 @@ typedef struct builtin_s
 char *shell_read_line(void);
 char **shell_split_line(char *line);
 int shell_execute(char **args, char *shell_name, int cmd_count);
+char *get_path(char *command)
 
 char *find_in_path(char *cmd);
 
-char *get_path(char *command)
 int shell_cd(char **args);
 int shell_help(char **args);
 int shell_exit(char **args);
